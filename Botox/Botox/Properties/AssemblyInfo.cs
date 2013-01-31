d66595ec9262107324d539f464eac95ce60b1162
projecteon <abspro@gmail.com>
2013-01-31 17:11:02 +0100

    Added nuspec and did project changes to release build for nuspec

--- Botox/Botox/Properties/AssemblyInfo.cs
+++ Botox/Botox/Properties/AssemblyInfo.cs
@@ -1,15 +1,13 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Botox")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Light weigth depency injection framework for the .NET portable class libraries.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aleksander Spro")]
 [assembly: AssemblyProduct("Botox")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
